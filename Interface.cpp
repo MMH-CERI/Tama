24d80e618c5cbb08e6adc9ab92c714e7a68ca4ca
MMH-CERI <dylan.hernandez@alumni.univ-avignon.fr>
2014-12-06 16:28:13 +0100

    MAJ

--- Interface.cpp
+++ Interface.cpp
@@ -6,10 +6,14 @@
 
 #include <SDL/SDL.h>
 #include "Constante.h"
+#include <string.h>
 
 int interface()
 {
-    system("xterm -e ./xt 1");
-    cout<<"interface";
+    /*system("xterm -e ./xt 1");
+    cout<<"interface";*/
+    string cmd = "xterm -e sh-c 'ls-l; exec bash'";
+    cout<<"interface :"<<Jeu.c.vie<<endl;
+    system (cmd.c_str ());
     return 0;
 }
